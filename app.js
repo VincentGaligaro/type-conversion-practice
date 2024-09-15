2f3e9caeca2c8c38d4f391b7bc54eabedcf575aa
VincentGaligaro <jhangvincent19@gmail.com>
2024-09-15 15:51:18 +0800

    Update app.js
    app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+let price = "200";
 
+let priceAsInt = parseInt(price);
+
+console.log(priceAsInt); // Output: 200
+
+let priceAsNumber = Number(price);
+
+console.log(priceAsNumber); // Output: 200
+
+let priceAsPlus = +price;
+
+console.log(priceAsPlus); // Output: 200
+
+let distance = 150;
+
+let distanceAsString1 = distance.toString();
+console.log(distanceAsString1); // Output: "150"
+
+let distanceAsString2 = String(distance);
+console.log(distanceAsString2); // Output: "150"
+
+let weight = "72.5";
+
+let weightAsFloat = parseFloat(weight);
+console.log(weightAsFloat); // Output: 72.5
